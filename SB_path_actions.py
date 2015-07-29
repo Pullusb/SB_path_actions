6fa4bd65abc6660f77800b9a92d5307336a14138
Pullusb <bernou.samuel@gmail.com>
2015-07-29 19:24:34 +0200

    update_browser_actions

--- SB_path_actions.py
+++ SB_path_actions.py
@@ -19,17 +19,83 @@
 bl_info = {
     "name": "Path Actions",
     "author": "Samuel Bernou",
-    "version": (1, 0),
-    "blender": (2, 73, 0),
-    "location": "Properties > Render > Output ",
+    "version": (1, 3),
+    "blender": (2, 75, 0),
+    "location": "Properties > Render > Output and filebrowser",
     "description": "open output path or blend file location in OS explorer",
     "warning": "",
     "wiki_url": "",
     "tracker_url": "",
     "category": "System"}
 
+"""
+Usage:
+
+Add buttons in properties > render > Output pannel and filebrowser
+quick-use: search "folder" in the spacebar menu
+"""
+
 import bpy
-from os import startfile, path
+from sys import platform
+from os import path, system
+
+def openFolder(folderpath):
+    """
+    open the folder at the path given
+    with cmd relative to user's OS
+    """
+    myOS = platform
+    if myOS.startswith('linux') or myOS.startswith('freebsd'):
+        # linux
+        cmd = 'xdg-open '
+        #print("operating system : Linux")
+    elif myOS.startswith('win'):
+        # Windows
+        #cmd = 'start '
+        cmd = 'explorer '
+        #print("operating system : Windows")
+        if not folderpath:
+            return('/')
+
+    else:#elif myOS == "darwin":
+        # OS X
+        #print("operating system : MACos")
+        cmd = 'open '
+
+    if not folderpath:
+        return('//')
+
+    #double quote the path to avoid problem with special character
+    folderpath = '"' + folderpath + '"'
+    fullcmd = cmd + folderpath
+
+    #print & launch open command
+    print(fullcmd)
+    system(fullcmd)
+
+
+class BrowserToBlendFolder(bpy.types.Operator):
+    """current blend path to the browser filepath"""
+    bl_idname = "path.paste_path"
+    bl_label = "Browser to blend folder"
+
+    def execute(self, context):
+        bpy.ops.file.select_bookmark(dir="//")
+        return {"FINISHED"}
+
+class OpenFilepathFolder(bpy.types.Operator):
+    """Open browser filepath directory in OS explorer"""
+    bl_idname = "path.open_filepath"
+    bl_label = "Open Current filepath"
+
+    def execute(self, context):
+        try: #handle error if context isn't filebrowser (if operator is launched from search)
+            folder = context.space_data.params.directory #give the path, params.filename give the tail (file)
+            openFolder(folder)
+        except:
+            self.report({'WARNING'}, "Only works in fileBrowser context")
+        return {"FINISHED"}
+
 
 class OpenBlendFolder(bpy.types.Operator):
     """open blend's directory in OS explorer"""
@@ -40,11 +106,10 @@ class OpenBlendFolder(bpy.types.Operator):
     def execute(self, context):
         fileloc = bpy.context.blend_data.filepath
         folder = path.dirname(fileloc)
-        startfile(folder)
+        openFolder(folder)
          
         self.report({'INFO'}, "Blend's folder opened")
         return {'FINISHED'}
-   
    
 
 class OpenOutputFolder(bpy.types.Operator):
@@ -65,11 +130,11 @@ class OpenOutputFolder(bpy.types.Operator):
         
         if path.exists(folder):
             self.report({'INFO'}, "Open " + folder)
-            startfile(folder)
+            openFolder(folder)
 
         elif path.exists(fhead):
            self.report({'INFO'}, "Open " + fhead)
-           startfile(fhead)
+           openFolder(fhead)
 
         else:
             self.report({'INFO'}, "Can't open " + folder)
@@ -84,9 +149,10 @@ class SwitchPathOperator(bpy.types.Operator):
     bl_options = {'REGISTER'}
     
     def execute(self, context):
+        compare = openFolder(False) # return "//" for windows, "/" for other OS
         
         fpath = bpy.context.scene.render.filepath
-        if fpath.startswith("/"):
+        if fpath.startswith(compare):
             bpy.context.scene.render.filepath = path.realpath(bpy.path.abspath(fpath))
         else:
             bpy.context.scene.render.filepath = bpy.path.relpath(fpath)
@@ -107,19 +173,41 @@ def PathActionsPanel(self, context):
     layout.operator(SwitchPathOperator.bl_idname, text = "Toggle path mode", icon = 'PARTICLE_PATH')
 
 
+def BrowserPathActionsButtons(self, context):
+    layout = self.layout
+    #split = layout.split()
+    row = layout.row(align=True)
+    if bpy.data.filepath:
+        #button to paste blend path in blender filebrowser's path (file must be saved for the button to appear)
+        row.operator(
+            BrowserToBlendFolder.bl_idname,
+            text="Blend location to filepath",
+            icon="APPEND_BLEND")
+    #button to open current filepath destination in OS browser
+    row.operator(
+        OpenFilepathFolder.bl_idname,
+        text="Open filepath folder",
+        icon="FILE_FOLDER")
+
 ################## Registration
 
 def register():
     bpy.utils.register_class(SwitchPathOperator)
     bpy.utils.register_class(OpenOutputFolder)
     bpy.utils.register_class(OpenBlendFolder)
+    bpy.utils.register_class(BrowserToBlendFolder)
+    bpy.utils.register_class(OpenFilepathFolder)
+    bpy.types.FILEBROWSER_HT_header.append(BrowserPathActionsButtons)
     bpy.types.RENDER_PT_output.append(PathActionsPanel)
 
 def unregister():
-    bpy.utils.unregister_class(SwitchPathOperator)
     bpy.utils.unregister_class(OpenOutputFolder)
     bpy.utils.unregister_class(OpenBlendFolder)
+    bpy.utils.unregister_class(BrowserToBlendFolder)
+    bpy.utils.unregister_class(OpenFilepathFolder)
+    bpy.utils.unregister_class(SwitchPathOperator)
     bpy.types.RENDER_PT_output.remove(PathActionsPanel)
+    bpy.types.FILEBROWSER_HT_header.remove(BrowserPathActionsButtons)
     
 if __name__ == "__main__":
     register()
